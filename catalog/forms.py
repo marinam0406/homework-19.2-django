ac493361c6d813299a4775b88e299621f4e20c11
marinam0406 <marinam0406@yandex.ru>
2024-06-04 22:18:36 +0300

    add homework 23.1

--- catalog/forms.py
+++ catalog/forms.py
@@ -7,7 +7,7 @@ from common.views import StyleFormMixin
 class ProductForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ('name', 'description', 'image', 'price', 'category')
 
     def clean_name(self):
         cleaned_data = self.cleaned_data.get('name')
@@ -24,6 +24,14 @@ class ProductForm(StyleFormMixin, forms.ModelForm):
             if word in cleaned_data:
                 raise forms.ValidationError('Недопустимое значение')
         return cleaned_data
+
+
+
+class ModeratorProductForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ('category', 'description', 'in_published')
+
 
 
 class VersionForm(StyleFormMixin, forms.ModelForm):
